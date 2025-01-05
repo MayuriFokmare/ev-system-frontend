e3f0d509e3d2358806c7688de51cad96b9ca3153
MayuriFokmare <41722994+MayuriFokmare@users.noreply.github.com>
2025-01-05 12:27:42 +0000

    googlemap

---
+++ src/assets/environment.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    googleMapsApiKey: 'AIzaSyDwtQNKiJQJIL-3a5YvkzV8x3okGgvy4VQ',  // Replace with your actual Google Maps API key
+  };
+  
